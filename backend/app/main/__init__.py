f8cc1e866fb00bad871efd9b429c583bbd082557
Kaung Min Khant <phenomenalkaung@gmail.com>
2024-11-16 20:42:20 +0700

    pull master

--- backend/app/main/__init__.py
+++ backend/app/main/__init__.py
@@ -12,6 +12,7 @@ from typing import Optional
 import os
 import uuid
 from .wallet import router as wallet_router
+from .users import router as login_router
 
 SECRET_KEY = os.getenv("SECRET_KEY", "your_secret_key")
 ALGORITHM = "HS256"
@@ -45,3 +46,4 @@ def get_db():
         db.close()
 
 app.include_router(wallet_router, prefix="/api/v1")
+app.include_router(login_router, prefix="/api/v1")
