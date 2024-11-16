65695473929401ac15cdd640547d14dc73493bef
Kaung Min Khant <phenomenalkaung@gmail.com>
2024-11-16 12:33:28 +0700

    clean pycache

---
+++ clean_pycache.sh
@@ -0,0 +1,6 @@
+rm -rf backend/app/crud/__pycache__/
+rm -rf backend/app/database/__pycache__/
+rm -rf backend/app/models/__pycache__/
+rm -rf backend/app/schemas/__pycache__/
+rm -rf backend/app/main/__pycache__/
+rm -rf backend/app/__pycache__/
